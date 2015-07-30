42ab6adc098137e812e9415f71ca145d28f0e9ab
Anselmo Fresquez III <anselmo.fresquez@gmail.com>
2015-07-29 22:42:25 -0700

    init

---
+++ Grades/Grades/GradeBook.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Grades {
+    class GradeBook {
+        List<float> Grades = new List<float>();
+
+        public void AddGrade (float grade) {
+            Grades.Add(grade);
+        }
+
+        public float AverageGrade () {
+            float sum = 0;
+
+            foreach (float grade in Grades) {
+                sum += grade;    
+            }
+
+            return sum / Grades.Count;
+        }
+
+        public float HighestGrade () {
+            float highestGrade = 0;
+
+            foreach (float grade in Grades) {
+                if (grade > highestGrade) {
+                    highestGrade = grade;
+                }
+            }
+
+            return highestGrade;
+        }
+
+        public float LowestGrade () {
+            float lowestGrade = -1;
+
+            foreach (float grade in Grades) {
+                if (lowestGrade == -1 || grade < lowestGrade) {
+                    lowestGrade = grade;
+                }
+            }
+
+            return lowestGrade;
+        }
+
+        public float PointScale () {
+            float pointScale = 0;
+            float averageGrade = AverageGrade();
+
+            pointScale = averageGrade / 22.5f;
+
+            return Math.Min(pointScale, 4.0f);
+        }
+
+        public List<float> GetGrades () {
+            return Grades;
+        }
+    }
+}
---
+++ Grades/Grades/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Grades {
+    class Program {
+        static void Main (string[] args) {
+            GradeBook GradeBook = new GradeBook();
+            Random Random = new Random();
+
+            for (int i = 0; i < 6; i++) {
+                GradeBook.AddGrade(Random.Next(75, 100));
+            }
+
+            Console.WriteLine("Student's grades:");
+
+            foreach (float grade in GradeBook.GetGrades()) {
+                Console.WriteLine(grade);
+            }
+
+            Console.WriteLine("Average grade:" + GradeBook.AverageGrade());
+            Console.WriteLine("Highest grade:" + GradeBook.HighestGrade());
+            Console.WriteLine("Lowest grade:" + GradeBook.LowestGrade());
+            Console.WriteLine("GPA: " + GradeBook.PointScale().ToString("#0.0"));
+
+            Console.Read();
+        }
+    }
+}
