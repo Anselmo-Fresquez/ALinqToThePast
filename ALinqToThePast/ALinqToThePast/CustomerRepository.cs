7e9755f75de5188797e40be6669914dd89e39e8c
Anselmo Fresquez III <anselmo.fresquez@gmail.com>
2015-07-31 07:20:51 -0700

    pm

---
+++ ALinqToThePast/ALinqToThePast/CustomerRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ALinqToThePast {
+    class CustomerRepository {
+        List<Customer> custList = new List<Customer> {
+            new Customer() {
+                CustomerId      = 1,
+                FirstName       = "Frodo",
+                LastName        = "Baggins",
+                EmailAddress    = "frobro@shiremail.nz",
+                CustomerTypeId  = 1
+            },
+            new Customer() {
+                CustomerId      = 2,
+                FirstName       = "Bilbo",
+                LastName        = "Baggins",
+                EmailAddress    = "goodmorning@shiremail.nz",
+                CustomerTypeId  = 1
+            },
+            new Customer() {
+                CustomerId      = 3,
+                FirstName       = "Samwise",
+                LastName        = "Baggins",
+                EmailAddress    = "goodmorning@shiremail.nz",
+                CustomerTypeId  = 1
+            },
+            new Customer() {
+                CustomerId      = 2,
+                FirstName       = "Rosie",
+                LastName        = "Cotton",
+                EmailAddress    = "goodmorning@shiremail.nz",
+                CustomerTypeId  = 2
+            }
+        };
+    }
+}
